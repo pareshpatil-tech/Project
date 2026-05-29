2abc07694cadb817f4dffa3eb9a913f8d320aa1b
Paresh patil <Your-email@example.com>
2026-05-29 11:17:40 +0530

    Project

--- Project09.java
+++ Project09.java
@@ -1,23 +1,64 @@
+import java.util.Scanner;
+
 class Person {
+    Scanner sc = new Scanner(System.in);
+    String History ="";
     private String name;
     private int age;
     private double amount;
-    private int accountNumber;
     private double balance;
-    private double interestRate;
-    private double overdraftLimit;
+    
 
 public void getDetails() {
     System.out.println("Name:"+ name);
     System.out.println("Age:"+ age);
-    System.out.println("Amount: "+ amount);
+    System.out.println("Available Balanace: "+ balance + " Rs.");
+    
    
    }
-public void setDetails(String name, int age, double amount) {
+public void setDetails(String name, int age, double balance) {
     this.name= name;
     this.age=age;
-    this.amount=amount;
+    this.balance=balance;
   }
+
+public void deposit(double amount) {
+    if(amount<=0) {
+        System.out.println("Invalid deposit amount. please enter a correct amount.");
+        amount = sc.nextDouble();
+    }
+    else {
+        balance += amount;
+        System.out.println("--Deposit succesfull.--");
+        System.out.println("New balance: "+ balance);
+        History += "Deposited: "+amount+"Rs. \n";
+    }
+} 
+
+
+public void withdraw(double amount) {
+    if(amount<0) {
+        System.out.println("Invalid Amount. Please enter the coorect amount.");
+    }
+    else if(amount>balance) {
+        System.out.println("Insufficient balance. Please enter the correct amount.");
+
+    }
+    else {
+        balance-= amount;
+        System.out.println("--Withdrawal Succesfull.--");
+        System.out.println("Updated Balance: "+balance);
+        History += "Withdrew: "+amount+"Rs. \n";
+    }
+  }
+
+public void TransactionHistory() {
+    System.out.println("-----Transaction History-----");
+    System.out.println(History);
+    
+}
+
+ 
 }
 //Bank Management System...
 
@@ -25,6 +66,8 @@ public void setDetails(String name, int age, double amount) {
 class Account extends Person {
     int  accountNumber;
     double balance;
+    double deposit;
+    double withdraw;
 }
 class SavingsAccount extends Account {
     double interestRate;
@@ -36,22 +79,25 @@ class CurrentAccount extends Account {
 public class Project09 {
     public static void main(String[] args) {
         SavingsAccount s1 = new SavingsAccount();
-        s1.setDetails("Paresh PAtil", 19, 12009.56);
+        s1.setDetails("Paresh Patil", 19, 2000);
         s1.accountNumber=602435678;
-        s1.balance=45677899.78;
+        s1.balance=2000;
         s1.interestRate=5.9;
-        System.out.println("\n --Savings Account Details--");
+        System.out.println("\n -----Savings Account Details-----");
+        s1.deposit(2500);
+        s1.withdraw(500);
+        s1.TransactionHistory();
         s1.getDetails();
-
+       
         CurrentAccount c1 = new CurrentAccount();
-        c1.setDetails("Shlok Patil", 18, 34000.78);
-        c1.accountNumber=45678934;
-        c1.balance=560000.45;
-        System.out.println("\n --Current Account Details--");
+        c1.setDetails("Shlok Patil", 18, 4000);
+        c1.accountNumber=400056789;
+        c1.balance=4000;
+        System.out.println("\n -----Current Account Details-----");
         c1.overdraftLimit=1000.98;
+        c1.deposit(4000);
+        c1.withdraw(1000);
+        c1.TransactionHistory();
         c1.getDetails();
-
-
-
     }
 }
